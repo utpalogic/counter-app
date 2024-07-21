e200aba3bf23448d6fc4cc33052f386100d36a3d
Utpala khatri <141339568+utpalogic@users.noreply.github.com>
2024-07-21 18:49:53 +0545

    first counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState(0);
+
+  const plusNum = () => {
+    setNum(num + 1);
+  };
+
+  const minusNum = () => {
+    setNum(num - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main_div'>
+        <div className='center_div'>
+          <h2 className='heading'>Welcome to counter app</h2>
+          <div className='num-display'>{num}</div>
+          <div className='button_section'>
+            <button onClick={plusNum} className='but'>+</button>
+            <button onClick={minusNum} className='but'>-</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
